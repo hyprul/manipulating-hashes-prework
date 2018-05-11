58138cfdd218a70c98af7f4d16ebafa3babd96d7
Sean Huang <hyprul@ucla.edu>
2018-05-10 23:48:14 -0500

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,36 @@ def first_challenge
   }
 
   #your code here
+  #contacts["Jon Snow"][:favorite_icecream_flavors] << "mint chip"
+  #contacts["Jon Snow"][:address] = "The Lord Commander's Rooms, The Wall, Westeros"
 
+=begin
+contacts.each do |person, data|
+  #at this level, "person" is Jon Snow or Freddy and "data" is a hash of key/value pairs
+  #to iterate over the "data" hash, we can use the following line: 
+ 
+  data.each do |attribute, value|
+    #at this level, "attribute" is describes the key of :name, :email, :favorite_icecream_flavors, or :knows
+    #we need to first check and see if the key is :favorite_icecream_flavors,
+    #if it is, that means the VALUE is an array that we can iterate over to print out each element
+ 
+    if attribute == :favorite_icecream_flavors
+      value.each do |flavor|
+        # here, each index element in an ice cream flavor string
+        puts "#{flavor}"
+      end
+    end
+  end
+end
+=end
+  
+  contacts["Freddy Mercury"].each do |attribute, value|
+      if attribute == :favorite_icecream_flavors
+        value.delete_if do |flavor|
+          flavor == "strawberry"
+      end
+    end
+  end
 
   #remember to return your newly altered contacts hash!
   contacts
